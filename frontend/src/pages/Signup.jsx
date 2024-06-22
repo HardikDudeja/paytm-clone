0234c8f748bb6bbed29724c8a6bc9d25410e3237
HardikDudeja <hardik.dudeja39@gmail.com>
2024-06-22 22:59:16 +0530

    Signup endpoint integrated with backend

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,23 +1,37 @@
+import { useState } from "react";
 import { BottomWarning } from "../components/BottomWarning";
 import { Button } from "../components/Button";
 import { DudejaLogo } from "../components/DudejaLogo";
 import { Heading } from "../components/Heading";
 import { InputBox } from "../components/InputBox";
 import { SubHeading } from "../components/SubHeading";
+import axios from "axios";
 
 export const Signup = () => {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className="bg-black h-screen flex">
       <div className="flex w-2/3 items-center">
         <div className="rounded-lg bg-white p-2 px-4 w-full">
           <Heading label={"Sign up"} />
           <SubHeading label={"Enter your infromation to create an account"} />
-          <InputBox label={"First Name"} />
-          <InputBox label={"Last Name"} />
-          <InputBox label={"Email"} />
-          <InputBox label={"Password"} />
+          <InputBox onChange = {(e) => setFirstName(e.target.value)} label={"First Name"} />
+          <InputBox onChange = {(e) => setLastName(e.target.value)} label={"Last Name"} />
+          <InputBox onChange = {(e) => setUserName(e.target.value)} label={"Email"} />
+          <InputBox onChange = {(e) => setPassword(e.target.value)} label={"Password"} />
           <div className="pt-4">
-            <Button label={"Sign up"} />
+            <Button onClick={() => axios.post("http://localhost:3000/api/v1/user/signup",
+              {
+                userName,
+                password, 
+                firstName, 
+                lastName
+              }
+            ).then((response) => console.log("response", response)).catch((e) => console.log("error", e))} label={"Sign up"} />
           </div>
           <BottomWarning
             label={"Already have an account?"}
