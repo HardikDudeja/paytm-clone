5da0044b793110a45c29eb229e94e53022bf7f0b
HardikDudeja <hardik.dudeja39@gmail.com>
2024-06-22 18:25:43 +0530

    Add signin page

---
+++ frontend/src/components/DudejaLogo.jsx
@@ -0,0 +1,9 @@
+export function DudejaLogo(){
+    return (
+        <img
+            className="px-8 hover:px-4 transition-all duration-300"
+            src="/dudeja-high-resolution-logo-white-transparent.png"
+            alt="Name logo"
+        />
+    )
+}
--- frontend/src/pages/Signin.jsx
+++ frontend/src/pages/Signin.jsx
@@ -5,25 +5,19 @@ import { Heading } from "../components/Heading";
 import { InputBox } from "../components/InputBox";
 import { SubHeading } from "../components/SubHeading";
 
-export const Signup = () => {
+export const Signin = () => {
   return (
     <div className="bg-black h-screen flex">
       <div className="flex w-2/3 items-center">
         <div className="rounded-lg bg-white p-2 px-4 w-full">
-          <Heading label={"Sign up"} />
-          <SubHeading label={"Enter your infromation to create an account"} />
-          <InputBox label={"First Name"} />
-          <InputBox label={"Last Name"} />
-          <InputBox label={"Email"} />
-          <InputBox label={"Password"} />
-          <div className="pt-4">
-            <Button label={"Sign up"} />
-          </div>
-          <BottomWarning
-            label={"Already have an account?"}
-            buttonText={"Sign in"}
-            to={"/signin"}
-          />
+            <Heading label={"Sign in"} />
+            <SubHeading label={"Enter your credentials to access your account"} />
+            <InputBox label={"Email"} />
+            <InputBox label={"Password"} />
+            <div className="pt-4">
+                <Button label={"Sign in"} />
+            </div>
+            <BottomWarning label={"Don't have an account?"} buttonText={"Sign up"} to={"/signup"} />
         </div>
       </div>
       <div className="flex items-center w-1/3">
