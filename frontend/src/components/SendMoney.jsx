080c47653ca9bd4748f1da51eeb5842f8556982d
HardikDudeja <hardik.dudeja39@gmail.com>
2024-06-26 00:39:12 +0530

    Send money functionality added- application almost made

--- frontend/src/components/SendMoney.jsx
+++ frontend/src/components/SendMoney.jsx
@@ -1,4 +1,13 @@
+import axios from "axios";
+import { useState } from "react";
+import { useSearchParams } from "react-router-dom"
+
 export const SendMoney = () => {
+    const [searchParams] = useSearchParams();
+    const to = searchParams.get('id');
+    const name = searchParams.get('name');
+    const [amount, setAmount] = useState(0);
+
     return <div class="flex justify-center h-screen bg-gray-100">
         <div className="h-full flex flex-col justify-center">
             <div
@@ -10,9 +19,9 @@ export const SendMoney = () => {
                 <div class="p-6">
                 <div class="flex items-center space-x-4">
                     <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                    <span class="text-2xl text-white">A</span>
+                    <span class="text-2xl text-white">{name[0]}</span>
                     </div>
-                    <h3 class="text-2xl font-semibold">Friend's Name</h3>
+                    <h3 class="text-2xl font-semibold">{name}</h3>
                 </div>
                 <div class="space-y-4">
                     <div class="space-y-2">
@@ -27,9 +36,16 @@ export const SendMoney = () => {
                         class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
                         id="amount"
                         placeholder="Enter amount"
+                        onChange={(e) => setAmount(Number(e.target.value))}
                     />
                     </div>
-                    <button class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
+                    <button onClick={async () => {
+                        localStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2Njc2YzNiNzY2ZjcxZTk1MTljOTE1OGQiLCJpYXQiOjE3MTkzNDE0NDZ9.FENTbFgKzcLVB5XPyrxxei8TfFez_DVvnxxOB5L55YU')
+                        const response = await axios.post("http://localhost:3000/api/v1/account/transfer", {to, amount}, {headers: {
+                            'Authorization': `Bearer ${localStorage.getItem('token')}`
+                        }});
+                        console.log("printing response", response);
+                    }} class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
                         Initiate Transfer
                     </button>
                 </div>
